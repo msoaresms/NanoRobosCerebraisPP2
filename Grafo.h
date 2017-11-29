10cee6990368da74ac913abc28979a32f5e0cd65
Matheus Soares <m.soares.ms@outlook.com>
2017-11-29 01:34:25 -0400

    remoção de arquivos desnecesários

--- Grafo.h
+++ Grafo.h
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <vector>
-#include <iomanip> //será removido--------------------------------------------------------------------------------------
 using namespace std;
 
 template <class T>
@@ -23,11 +22,9 @@ public:
 
     void inicializar();
     void inserirAresta(int, int, float);
-    void mostrar();
     void inserirItem(T, int);
     T getItem(int);
 
-    vector<T> getItens() { return this->itens; }
     vector<float> *getMatriz() { return this->matriz; }
     int getOrdem() { return this->ordem; }
     int getTamanho() { return this->tamanho; }
@@ -37,11 +34,12 @@ template <typename T>
 void Grafo<T>::inicializar() {
     this->itens.resize(this->ordem+1);
     this->matriz = new vector<float>[this->ordem+1];
+
     for (int i = 0; i <= this->ordem; i++) { matriz[i].assign(this->ordem+1, 0); }
 }
 
 template <typename T>
-void Grafo<T>::inserirAresta(int u, int v, float w){
+void Grafo<T>::inserirAresta(int u, int v, float w) {
     this->matriz[u][v] = w;
     this->matriz[v][u] = w;
     this->tamanho++;
@@ -51,31 +49,6 @@ template <typename T>
 void Grafo<T>::inserirItem(T pItem, int pVertice) { this->itens[pVertice] = pItem; }
 
 template <typename T>
-T Grafo<T>::getItem(int n) {
-    return this->itens[n];
-}
-
-//será removido---------------------------------------------------------------------------------------------------------
-template <typename T>
-void Grafo<T>::mostrar() {
-    int k = 8;
-    cout << "   ";
-    for (int j = 1; j <= this->ordem; j++){
-        cout << setw(k) << j;
-    }
-    cout << endl;
-    for (int j = 1; j <= (this->ordem*k+3); j++){
-        cout << "-";
-    }
-    cout << endl;
-    for (int i = 1; i <= this->ordem; i++){
-        cout << setw(1) << i;
-        cout << " |";
-        for (int j = 1; j <= this->ordem; j++){
-            cout << setw(k) << matriz[i][j];
-        }
-        cout << endl;
-    }
-}
+T Grafo<T>::getItem(int n) { return this->itens[n]; }
 
 #endif //NANOROBOSCEREBRAIS_GRAFO_H
